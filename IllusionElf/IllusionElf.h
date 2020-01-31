89774f9f2f43bde2714e562fa31f4f68aeb269a5
Administrator <Administrator@WIN-1U6KA4DBV20>
2020-01-31 20:44:56 +0800

    调试

--- IllusionElf/IllusionElf.h
+++ IllusionElf/IllusionElf.h
@@ -4,12 +4,11 @@
 #include "Shadow.h"
 #include "PQEModel.h"
 
-class IllusionElf:public DimmensionWorldConsole
+class IllusionElf :public DimmensionWorldConsole
 {
 public:
 	IllusionElf();
 	~IllusionElf();
-
 
 	virtual void init()
 	{
@@ -17,7 +16,6 @@ public:
 		glEnable(GL_DEPTH_TEST);
 		SysVar.renderBasic.use();
 		SysVar.renderBasic.setBool("animat", true);
-		SysVar.renderBasic.setMat4("view", look.getViewMat());
 		SysVar.renderBasic.setVec3("carmePos", look.getView());
 		SysVar.renderBasic.setMat4("model", glm::rotate(translate(glm::scale(glm::mat4(1.0f), glm::vec3(0.01f)), glm::vec3(0., 0., 0.)), glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
 		SysVar.renderBasic.setMat4("lightSpaceMatrix", shadow.getLightSpaceMatrix());
@@ -25,21 +23,21 @@ public:
 		//IE_MODEL.load("miku.fbx");
 
 		model = new PQE::PQEModel("miku.fbx");
-		this->model->SetBoneSSAOId(SysVar.renderBasic.createShaderStorageBufferObject(1, sizeof(glm::mat4)*this->model->GetModel()->matrixNum,NULL,GL_DYNAMIC_COPY));
-		this->model->SetShapeSSAOId(SysVar.renderBasic.createShaderStorageBufferObject(2, sizeof(glm::vec3)*1000000, NULL, GL_DYNAMIC_COPY));
+		this->model->SetBoneSSAOId(SysVar.renderBasic.createShaderStorageBufferObject(1, sizeof(glm::mat4)*this->model->GetModel()->matrixNum, NULL, GL_DYNAMIC_COPY));
+		this->model->SetShapeSSAOId(SysVar.renderBasic.createShaderStorageBufferObject(2, sizeof(glm::vec3) * 1000000, NULL, GL_DYNAMIC_COPY));
 	}
 
 	virtual void renderBasis()
 	{
 		if (i > 360.f)
 			i = 0;
-		i+=0.05f;
+		i += 0.05f;
 		SysVar.renderBasic.use();
 		SysVar.renderBasic.setMat4("view", look.getViewMat());
 		SysVar.renderBasic.setVec3("carmePos", glm::vec3(0.0f));
 		SysVar.renderBasic.setMat4("model", glm::rotate(glm::rotate(glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(0.1f)), glm::vec3(0.0f, -100.0f, -100.0f)), glm::radians(i), glm::vec3(0.0f, 1.0f, 0.0f)), glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
 		model->Render(&SysVar.renderBasic);
- 		//look.useMouse();
+		//look.useMouse();
 
 	}
 private:
@@ -48,5 +46,4 @@ private:
 	Shadow shadow;
 	PQE::PQEModel *model;
 	float i = 0;
-};
-
+}
